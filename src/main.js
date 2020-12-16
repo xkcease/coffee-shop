6ed24eac2b227728212dfd1a7907d28a61aeb5d4
xkcease <xkcease@163.com>
2020-12-16 10:15:55 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'amfe-flexible';
+import 'font-awesome/css/font-awesome.min.css';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueAxios, axios);
+axios.defaults.baseURL = 'http://www.kangliuyong.com:10002';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+    router,
+    store,
+    render: h => h(App),
+}).$mount('#app');
