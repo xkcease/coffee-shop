f2ce59d1369edc2ffc5c261c56733dc9cee72341
xkcease <xkcease@163.com>
2020-12-21 12:40:21 +0800

    add new route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import { Toast } from 'vant';
 
 Vue.use(VueRouter);
 
@@ -7,14 +8,68 @@ const routes = [
     {
         path: '/',
         alias: '/login',
-        name: 'login',
+        name: 'Login',
         component: () =>
             import(/* webpackChunkName: "login" */ '../views/Login'),
+    },
+    {
+        path: '/primary',
+        name: 'Primary',
+        component: () =>
+            import(/* webpackChunkName: "primary" */ '../views/Primary'),
+        children: [
+            {
+                path: 'home',
+                name: 'Home',
+                component: () =>
+                    import(
+                        /* webpackChunkName: "primary" */ '../views/primary/Home'
+                    ),
+            },
+            {
+                path: 'menu',
+                name: 'Menu',
+                component: () =>
+                    import(
+                        /* webpackChunkName: "primary" */ '../views/primary/Menu'
+                    ),
+            },
+            {
+                path: 'shoppingBag',
+                name: 'ShoppingBag',
+                component: () =>
+                    import(
+                        /* webpackChunkName: "primary" */ '../views/primary/ShoppingBag'
+                    ),
+            },
+            {
+                path: 'my',
+                name: 'My',
+                component: () =>
+                    import(
+                        /* webpackChunkName: "primary" */ '../views/primary/My'
+                    ),
+            },
+        ],
+    },
+    {
+        path: '/detail',
+        name: 'Detail',
+        component: () =>
+            import(/* webpackChunkName: "detail" */ '../views/Detail'),
     },
 ];
 
 const router = new VueRouter({
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.name !== 'Login') {
+        Toast.loading({ message: '加载中...', duration: 0, forbidClick: true });
+    }
+
+    next();
 });
 
 export default router;
