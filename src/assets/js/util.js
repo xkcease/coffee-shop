c7c27e2291e107e04b887068b72ca66f165728ad
xkcease <xkcease@163.com>
2020-12-25 13:55:37 +0800

    modify .js

--- src/assets/js/util.js
+++ src/assets/js/util.js
@@ -31,4 +31,24 @@ function debounce(callback, delay, ...rest) {
     };
 }
 
-export { queryString, throttle, debounce };
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+
+    let YY = date.getFullYear() + '-';
+    let MM =
+        (date.getMonth() + 1 < 10
+            ? '0' + (date.getMonth() + 1)
+            : date.getMonth() + 1) + '-';
+    let DD = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+    let hh =
+        (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
+    let mm =
+        (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) +
+        ':';
+    let ss =
+        date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+
+    return YY + MM + DD + ' ' + hh + mm + ss;
+}
+
+export { queryString, throttle, debounce, formatDate };
